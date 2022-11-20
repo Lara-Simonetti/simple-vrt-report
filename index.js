8e207b8f70308fd6a2fe2cc5a28cae5b0b94b082
Lara <the.lemonetti@gmail.com>
2022-11-19 23:23:21 -0800

    Se añade la segunda carpeta de pruebas

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const compareImages = require("resemblejs/compareImages");
 steps = [];
 
 function getSteps(){
-    var files = fs.readdirSync('./results/');
+    var files = fs.readdirSync('./first-results/');
     for(let i = 0; i < files.length; i++){
         if(files[i].slice(0,5) == "after"){
             steps.push(files[i].slice(6).slice(0, -4));
@@ -17,8 +17,8 @@ async function executeComparison(){
     let resultInfo = {}
    for(let i = 0; i < steps.length; i++){
         const data = await compareImages(
-            fs.readFileSync(`./results/before-${steps[i]}.png`),
-            fs.readFileSync(`./results/after-${steps[i]}.png`)
+            fs.readFileSync(`./first-results/before-${steps[i]}.png`),
+            fs.readFileSync(`./first-results/after-${steps[i]}.png`)
             
         );
         resultInfo[steps[i]] = {
@@ -29,7 +29,7 @@ async function executeComparison(){
             diffBounds: data.diffBounds,
             analysisTime: data.analysisTime
         }
-        fs.writeFileSync(`./results/compare-${steps[i]}.png`, data.getBuffer());
+        fs.writeFileSync(`./first-results/compare-${steps[i]}.png`, data.getBuffer());
     }
     console.log('------------------------------------------------------------------------------------')
     console.log("Execution finished. Check the report under the results folder")
