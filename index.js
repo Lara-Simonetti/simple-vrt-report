d9fb0672462c4ef85cc99e0381f0e0179083a2f5
Lara <the.lemonetti@gmail.com>
2022-11-20 00:53:19 -0800

    Se termina el formato del reporte

--- index.js
+++ index.js
@@ -38,27 +38,42 @@ function comparisonsHTML(){
     folders.push("final-results");
     result = "";
     for(let i = 0; i < steps.length; i++){
-        result+=(`<div id="comparison-${steps[i]}">
-        <div id = "first">
-            <img class="img2" src="./first-results/${steps[i]}.png" id="firstImage" label="Primera prueba ejecutada">
-        </div>
-        <div id = "second">
-            <img class="img2" src="./second-results/${steps[i]}.png" id="secondImage" label="Segunda prueba ejecutada">
-        </div>
-        <div id = "final">
-            <img class="img2" src="./final-results/${steps[i]}.png" id="finalImage" label="Comparación entre ambas pruebas">
-        </div>`);
+        result+=(`<div id="comparison-${steps[i]}" class="container">
+    <h2>Regresión visual: ${steps[i]}</h2>
+    <hr>
+        <div class = "row">
+                <div id = "first" class="col-md-4">
+                    <h3><small class="text-muted">Primera prueba ejecutada</small></h3>
+                    <img class="img-fluid" src="./first-results/${steps[i]}.png" id="firstImage" label="Primera prueba ejecutada">
+                </div>
+                <div id = "second" class="col-md-4">
+                    <h3><small class="text-muted">Segunda prueba ejecutada</small></h3>
+                    <img class="img-fluid" src="./second-results/${steps[i]}.png" id="secondImage" label="Segunda prueba ejecutada">
+                </div>
+                <div id = "final" class="col-md-4">
+                    <h3><small class="text-muted">Comparación</small></h3>
+                    <img class="img-fluid" src="./final-results/${steps[i]}.png" id="finalImage" label="Comparación entre ambas pruebas">
+                </div>
+            </div>
+    </div>`);
     }
     return result;
 }
 
 function createReport(){
     comparisons = comparisonsHTML();
+    currentDate = new Date().toLocaleString();
+
     reportHTML = `<html>
     <head>
         <title> Reporte de Pruebas de Regresión Visual </title>
-        <link href="index.css" type="text/css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
+    <div class = "container">
+        <h1>Reporte de Pruebas de Regresión Visual</h1>
+        <h2><small class="text-muted">Generado: ${currentDate}</small></h2>
+        <hr>
+    </div>
     <body>
         ${comparisons}
     </body>
