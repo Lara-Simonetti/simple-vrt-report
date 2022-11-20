193a83cd30f5cec3ac5d21d84ad45a34e819aeb6
Lara <the.lemonetti@gmail.com>
2022-11-19 23:50:47 -0800

    Se edita el script para comparar imágenes en dos carpetas

--- index.js
+++ index.js
@@ -2,35 +2,39 @@ const config = require("./config.json");
 const fs = require('fs');
 const compareImages = require("resemblejs/compareImages");
 steps = [];
+folders = ["second-results", "first-results"]
 
 function getSteps(){
     var files = fs.readdirSync('./first-results/');
     for(let i = 0; i < files.length; i++){
-        if(files[i].slice(0,5) == "after"){
-            steps.push(files[i].slice(6).slice(0, -4));
-        }
+        steps.push(files[i].slice(0,-4));
     }
+    
+//    for(let i = 0; i < files.length; i++){
+//        if(files[i].slice(0,5) == "after"){
+//            steps.push(files[i].slice(6).slice(0, -4));
+//        }
+//    }
 }
 
 async function executeComparison(){
-    console.log("Inicia la comparación");
     let resultInfo = {}
-   for(let i = 0; i < steps.length; i++){
-        const data = await compareImages(
-            fs.readFileSync(`./first-results/before-${steps[i]}.png`),
-            fs.readFileSync(`./first-results/after-${steps[i]}.png`)
-            
-        );
-        resultInfo[steps[i]] = {
-            isSameDimensions: data.isSameDimensions,
-            dimensionDifference: data.dimensionDifference,
-            rawMisMatchPercentage: data.rawMisMatchPercentage,
-            misMatchPercentage: data.misMatchPercentage,
-            diffBounds: data.diffBounds,
-            analysisTime: data.analysisTime
+    for(let i = 0; i < steps.length; i++){
+            const data = await compareImages(
+                fs.readFileSync(`./first-results/${steps[i]}.png`),
+                fs.readFileSync(`./second-results/${steps[i]}.png`)
+                
+            );
+            resultInfo[steps[i]] = {
+                isSameDimensions: data.isSameDimensions,
+                dimensionDifference: data.dimensionDifference,
+                rawMisMatchPercentage: data.rawMisMatchPercentage,
+                misMatchPercentage: data.misMatchPercentage,
+                diffBounds: data.diffBounds,
+                analysisTime: data.analysisTime
+            }
+            fs.writeFileSync(`./final-results/compare-${steps[i]}.png`, data.getBuffer());
         }
-        fs.writeFileSync(`./first-results/compare-${steps[i]}.png`, data.getBuffer());
-    }
     console.log('------------------------------------------------------------------------------------')
     console.log("Execution finished. Check the report under the results folder")
     return resultInfo;
