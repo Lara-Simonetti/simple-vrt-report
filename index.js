8e032635088c46ca719b1dcab8b842fb2ecbd7c5
Lara <the.lemonetti@gmail.com>
2022-12-31 00:01:30 -0800

    Made it easier to change the report images' file type

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const config = require("./config.json");
 const fs = require('fs');
 const compareImages = require("resemblejs/compareImages");
+
 steps = [];
-folders = ["second-results", "first-results"]
+folders = ['second-results', 'first-results']
+fileType = '.png'
 
 function getSteps(){
     var files = fs.readdirSync('./first-results/');
@@ -15,8 +17,8 @@ async function executeComparison(){
     let resultInfo = {}
     for(let i = 0; i < steps.length; i++){
             const data = await compareImages(
-                fs.readFileSync(`./first-results/${steps[i]}.png`),
-                fs.readFileSync(`./second-results/${steps[i]}.png`)
+                fs.readFileSync(`./first-results/${steps[i]}${fileType}`),
+                fs.readFileSync(`./second-results/${steps[i]}${fileType}`)
                 
             );
             resultInfo[steps[i]] = {
@@ -27,7 +29,7 @@ async function executeComparison(){
                 diffBounds: data.diffBounds,
                 analysisTime: data.analysisTime
             }
-            fs.writeFileSync(`./final-results/${steps[i]}.png`, data.getBuffer());
+            fs.writeFileSync(`./final-results/${steps[i]}${fileType}`, data.getBuffer());
         }
     console.log('------------------------------------------------------------------------------------')
     console.log("Execution finished. Check the report under the results folder")
@@ -39,19 +41,19 @@ function comparisonsHTML(){
     result = "";
     for(let i = 0; i < steps.length; i++){
         result+=(`<div id="comparison-${steps[i]}" class="container">
-    <h2 class="mt-2 mb-3">Regresión visual: ${steps[i]}</h2>
+    <h2 class="mt-2 mb-3">Visual Regression Testing: ${steps[i]}</h2>
         <div class = "row">
                 <div id = "first" class="col-md-4">
-                    <h3 class="mt-2 mb-3"><small class="text-muted">Primera prueba ejecutada</small></h3>
-                    <img class="img-fluid" src="./first-results/${steps[i]}.png" id="firstImage" label="Primera prueba ejecutada">
+                    <h3 class="mt-2 mb-3"><small class="text-muted">First Execution</small></h3>
+                    <img class="img-fluid" src="./first-results/${steps[i]}${fileType}" id="firstImage" label="Primera prueba ejecutada">
                 </div>
                 <div id = "second" class="col-md-4">
-                    <h3 class="mt-2 mb-3"><small class="text-muted">Segunda prueba ejecutada</small></h3>
-                    <img class="img-fluid" src="./second-results/${steps[i]}.png" id="secondImage" label="Segunda prueba ejecutada">
+                    <h3 class="mt-2 mb-3"><small class="text-muted">Second Execution</small></h3>
+                    <img class="img-fluid" src="./second-results/${steps[i]}${fileType}" id="secondImage" label="Segunda prueba ejecutada">
                 </div>
                 <div id = "final" class="col-md-4">
-                    <h3 class="mt-2 mb-3"><small class="text-muted">Comparación</small></h3>
-                    <img class="img-fluid" src="./final-results/${steps[i]}.png" id="finalImage" label="Comparación entre ambas pruebas">
+                    <h3 class="mt-2 mb-3"><small class="text-muted">Difference</small></h3>
+                    <img class="img-fluid" src="./final-results/${steps[i]}${fileType}" id="finalImage" label="Comparación entre ambas pruebas">
                 </div>
             </div>
             <hr>
@@ -66,12 +68,12 @@ function createReport(){
 
     reportHTML = `<html>
     <head>
-        <title> Reporte de Pruebas de Regresión Visual </title>
+        <title> Visual Regression Testing Report </title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <div class = "container">
-        <h1>Reporte de Pruebas de Regresión Visual</h1>
-        <h2><small class="text-muted">Generado: ${currentDate}</small></h2>
+        <h1> Visual Regression Testing Report </h1>
+        <h2><small class="text-muted">Date Generated: ${currentDate}</small></h2>
         <hr>
     </div>
     <body>
